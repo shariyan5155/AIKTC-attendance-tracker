ebea6a73fe8160137b0e59ad0fff0fab7a942b1d
shariyan5155 <shariyanshaikh5155@gmail.com>
2026-09-13 01:15:16 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'aik-attendance-v1';
+const assetsToCache = [
+  './',
+  './index.html',
+  './tracker.html',
+  './logo.png',
+  './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(assetsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
